c3ef6e620d4759bfc8043db5b86d058720a7ba23
Madison Hanberry <mhanberry1@gmail.com>
2019-12-29 02:32:15 -0500

    the singlularity container can now be executed without root privileges

--- ubuntu-singularity/1204/buildContainer.sh
+++ ubuntu-singularity/1204/buildContainer.sh
@@ -4,13 +4,19 @@
 set -e
 
 # Build the initial sandbox image
-singularity build --sandbox nmag-sandbox nmag.def
+singularity build --fakeroot --sandbox nmag-sandbox nmag.def
 
 # Compile the code within the sandbox
-singularity exec --writable nmag-sandbox/ bash -c 'cd /usr/share/nmag && make && make clean && ln -sf /usr/share/nmag/bin/* /usr/bin'
+singularity exec --writable nmag-sandbox/ bash -c '\
+	cd /usr/share/nmag && \
+	make && \
+	make clean && \
+	ln -sf /usr/share/nmag/bin/* /usr/bin && \
+	chmod -R 755 .
+'
 
 # Convert the sandbox image into a singularity container image
-singularity build nmag.sif nmag-sandbox/
+singularity build --fakeroot nmag.sif nmag-sandbox/
 
 # Clean up
 rm -rf nmag-*
